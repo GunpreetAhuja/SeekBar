49dc57962072615d60afae80c9d91f2548a68eac
GunpreetAhuja <gunpreetahuja0528@gmail.com>
2015-07-28 21:37:07 +0530

    SeekBar

---
+++ app/src/main/java/com/example/geenu/seekbar/MainActivity.java
@@ -0,0 +1,61 @@
+package com.example.geenu.seekbar;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.SeekBar;
+import android.widget.Toast;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    SeekBar s;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        s = (SeekBar)findViewById(R.id.seekBar);
+        s.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            int progressChanged = 0;
+
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                progressChanged = progress;
+            }
+
+                public void onStartTrackingTouch(SeekBar seekBar) {
+
+                }
+
+                public void onStopTrackingTouch(SeekBar seekBar) {
+                    Toast.makeText(MainActivity.this, "seek bar progress:" + progressChanged,
+                            Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
+
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
